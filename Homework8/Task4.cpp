d758db0527643783498d45fa1eff775eb3ce019e
Kalina Uzunova <kalina1406@gmail.com>
2021-11-30 09:20:42 +0200

    Final Task 4

--- Homework8/Task4.cpp
+++ Homework8/Task4.cpp
@@ -4,31 +4,27 @@
 
 using namespace std;
 
-void addClient(const long& index, long& duration, vector<long>& operators, vector<long>& count) {
-	pair<long, long> client(index, duration);
-	long currentOperator = index % operators.size() - 1;
+void addClient(vector<long>& operators, const long& moment, const long& duration,
+	const long& index, vector<long>& count)
+{
+	long opIndex = (index % operators.size()) - 1;
+
+	if (opIndex == -1)
+		opIndex = operators.size() - 1;
 
 	for (long i = 0; i < operators.size(); i++)
 	{
+		if (opIndex > operators.size() - 1)
+			opIndex = 0;
 
-		if (currentOperator == -1)
+		if (operators[opIndex] <= moment)
 		{
-			currentOperator = operators.size() - 1;
-		}
-
-		if (operators[currentOperator] == 0)
-		{
-			operators[currentOperator] = duration;
-			count[currentOperator]++;
+			operators[opIndex] = duration + moment;
+			count[opIndex]++;
 			break;
 		}
 		else
-		{
-			currentOperator++;
-			if (currentOperator > (operators.size() - 1))
-				currentOperator = 0;
-		}
-
+			opIndex++;
 	}
 
 }
@@ -36,30 +32,19 @@ void addClient(const long& index, long& duration, vector<long>& operators, vecto
 int main() {
 	long clients, operators;
 	cin >> clients >> operators;
-	vector<long> currentOperators(operators, 0);
+
+	long prevMoment = 1;
 	vector<long> count(operators, 0);
-	long lastIndex = 1;
+	vector<long> currentOperators(operators, 0);
+
 	for (long i = 0; i < clients; i++)
 	{
-		long index, duration;
-		cin >> index >> duration;
+		long moment, duration;
+		cin >> moment >> duration;
 
+		addClient(currentOperators, moment, duration, i + 1, count);
 
-		if (index != lastIndex)
-		{
-			long diff = index - lastIndex;
-			for (auto& op : currentOperators)
-			{
-				if (op != 0)
-					op -= diff;;
-			}
-		}
-
-		addClient(index, duration, currentOperators, count);
-
-		lastIndex = index;
-
-
+		prevMoment = moment;
 	}
 
 	long max = *max_element(count.begin(), count.end());
@@ -69,6 +54,5 @@ int main() {
 		if (count[i] == max)
 			cout << i << " ";
 	}
-
 	return 0;
 }
