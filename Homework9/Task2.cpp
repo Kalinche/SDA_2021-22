de767e2b672a0cc01e97334ab9b8014c304d1f0b
Kalina Uzunova <kalina1406@gmail.com>
2021-12-06 13:51:21 +0200

    Optimized Task2

--- Homework9/Task2.cpp
+++ Homework9/Task2.cpp
@@ -1,64 +1,68 @@
 #include <iostream>
-#include <unordered_map>
-#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+long Factorial(const long& n) {
+	if (n == 0)
+		return 1;
+	return n * Factorial(n - 1);
+}
+
 int main() {
-
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-
 	long n, k;
 	cin >> n >> k;
 
-	unordered_map<long, set<long>> animals;
+	vector<long> animals;
+
 	for (long i = 0; i < n; i++)
 	{
 		long weight;
 		cin >> weight;
-
-		auto it = animals.find(weight);
-		if (it == animals.end())
-		{
-			set<long> vec;
-			vec.insert(i);
-			animals.insert(make_pair(weight, vec));
-		}
-
-		else
-		{
-			unordered_map<long, set<long>>::iterator it = animals.find(weight);
-			it->second.insert(i);
-		}
+		animals.push_back(weight);
 	}
 
 	long count = 0;
+	map<long, long> counting;//index, current count
+	map<long, long> ys;//y value, y current count
 
-	unordered_map<long, set<long>>::iterator it;
-	for (it = animals.begin(); it != animals.end(); it++)
+	for (long i = 0; i < animals.size(); i++)
 	{
-		unordered_map<long, set<long>>::iterator second = animals.find(it->first * k);
-		unordered_map<long, set<long>>::iterator third = animals.find(it->first * k * k);
-		if (second != animals.end() &&
-			third != animals.end())
+		if (counting.find(animals[i]) == counting.end())
 		{
-			set<long>::iterator it2;
-			for (it2 = it->second.begin(); it2 != it->second.end(); it2++)
+			counting.insert(make_pair(animals[i], 1));
+			ys.insert(make_pair(k * animals[i], 0));
+		}
+		else
+		{
+			counting[animals[i]]++;
+		}
+
+		if (ys.find(animals[i]) != ys.end())
+		{
+			if (k != 1)
+				ys[animals[i]] += counting[animals[i] / k];
+			else
+				ys[animals[i]]++;
+		}
+
+		if (ys.find(animals[i] / k) != ys.end())
+		{
+			if (k != 1)
 			{
-				auto it3 = second->second.upper_bound(*it2);
-				if (it3 == second->second.end())
-					break;
-
-				for (it3; it3 != second->second.end(); it3++)
-				{
-					auto it4 = third->second.upper_bound(*it3);
-					if (it4 == third->second.end())
-						break;
-					count += distance(it4, third->second.end());
-				}
+				count += ys[animals[i] / k];
 			}
+		}
+	}
 
+	if (k == 1)
+	{
+		for (pair<long, long> y : ys)
+		{
+			if (y.second >= 3)
+				count += Factorial(y.second) / (6 * Factorial(y.second - 3));
 		}
 	}
 
