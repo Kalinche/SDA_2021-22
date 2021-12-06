72fd0f8039dfc074fed0eafc4344b7a493c61902
Kalina Uzunova <kalina1406@gmail.com>
2021-12-06 17:26:38 +0200

    Isolated k=1

--- Homework9/Task2.cpp
+++ Homework9/Task2.cpp
@@ -5,13 +5,13 @@
 
 using namespace std;
 
-long Factorial2(const long& n) {
+long Factorial(const long& n) {
 	if (n == 0)
 		return 1;
-	return n * Factorial2(n - 1);
+	return n * Factorial(n - 1);
 }
 
-int Task2() {
+int main() {
 	long n, k;
 	cin >> n >> k;
 
@@ -28,41 +28,56 @@ int Task2() {
 	map<long, long> counting;//index, current count
 	map<long, long> ys;//y value, y current count
 
-	for (long i = 0; i < animals.size(); i++)
+	if (k != 1)
 	{
-		if (counting.find(animals[i]) == counting.end())
+		for (long i = 0; i < animals.size(); i++)
 		{
-			counting.insert(make_pair(animals[i], 1));
-			ys.insert(make_pair(k * animals[i], 0));
-		}
-		else
-		{
-			counting[animals[i]]++;
-		}
-
-		if (ys.find(animals[i]) != ys.end())
-		{
-			if (k != 1)
-				ys[animals[i]] += counting[animals[i] / k];
+			if (counting.find(animals[i]) == counting.end())
+			{
+				counting.insert(make_pair(animals[i], 1));
+				ys.insert(make_pair(k * animals[i], 0));
+			}
 			else
-				ys[animals[i]]++;
-		}
+			{
+				counting[animals[i]]++;
+			}
 
-		if (ys.find(animals[i] / k) != ys.end())
-		{
-			if (k != 1)
+			if (ys.find(animals[i]) != ys.end())
+			{
+				ys[animals[i]] += counting[animals[i] / k];
+			}
+
+			if (ys.find(animals[i] / k) != ys.end())
 			{
 				count += ys[animals[i] / k];
 			}
 		}
 	}
 
-	if (k == 1)
-	{
-		for (pair<long, long> y : ys)
+	else {
+		for (long i = 0; i < animals.size(); i++) {
+			if (counting.find(animals[i]) == counting.end())
+			{
+				counting.insert(make_pair(animals[i], 1));
+				ys.insert(make_pair(k * animals[i], 0));
+			}
+			else
+			{
+				counting[animals[i]]++;
+			}
+
+			if (ys.find(animals[i]) != ys.end())
+			{
+				ys[animals[i]]++;
+			}
+		}
+
 		{
-			if (y.second >= 3)
-				count += Factorial2(y.second) / (6 * Factorial2(y.second - 3));
+			for (pair<long, long> y : ys)
+			{
+				if (y.second >= 3)
+					count += (Factorial(y.second) / (6 * Factorial(y.second - 3)));
+			}
 		}
 	}
 
